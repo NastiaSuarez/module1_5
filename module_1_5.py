8c3175428ec737f376d9d38c22c354c0a6fd6bf8
NastiaSuarez <vipautomatisch@gmail.com>
2024-10-24 14:38:51 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2.5, True, "apple"
+print(immutable_var)
+print(immutable_var[3])
+#immutable_var[3] = False
+mutable_list = [1, 34, 56, 67]
+mutable_list[3] = 789
+print(mutable_list)
